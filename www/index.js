bc544d25485f3bcc3bf373ef46b46765d188dda5
Chris Ortner <chris@codexfons.com>
2014-06-20 00:59:19 +0200

    chat works now

--- www/index.js
+++ www/index.js
@@ -7,6 +7,10 @@ function init() {
             return function () { showContent(contentName); };
         })(contentNames[i]);
     }
+
+    var chatForm = document.getElementById('chatForm');
+    chatForm.onsubmit = submitChat;
+    startChatRefresh();
 }
 
 function headerAside() {
@@ -45,4 +49,46 @@ function hideElement(elem) {
     setTimeout(function () {
         elem.style.display = 'none';
     }, 200);
+}
+
+function refreshChat() {
+    var chatContent = document.getElementById('chatContent');
+    var r = new XMLHttpRequest();
+    r.onreadystatechange = function () {
+        if (r.readyState == 4) {
+            chatContent.innerHTML = r.responseText;
+        }
+    };
+    r.open('GET', 'chat_content.html', true);
+    r.send();
+}
+
+var chatRefreshTimer = undefined;
+function startChatRefresh() {
+    chatRefreshTimer = setInterval(refreshChat, 1000);
+}
+
+function submitChat() {
+    var chatForm = document.getElementById('chatForm');
+    var r = new XMLHttpRequest();
+    r.onreadystatechange = function () {
+        if (r.readyState == 4) {
+            refreshChat();
+        }
+    };
+    r.open('POST', '/cgi-bin/psowrte.py', true);
+    r.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    r.send(getFormData());
+    return false;
+}
+
+function getFormData() {
+    var form = document.getElementById('chatForm');
+    var nickElem = form.getElementsByClassName('nickname')[0];
+    var dataElem = form.getElementsByClassName('message')[0];
+    var formData = 'name=' + encodeURIComponent(nickElem.value) +
+           '&data=' + encodeURIComponent(dataElem.value) +
+           '&color=def';
+    dataElem.value = '';
+    return formData;
 }
